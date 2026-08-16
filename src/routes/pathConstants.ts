6fe8ba28185b7ce5be9ebc793f8012dab798baed
CrawFish666 <emelyakoro4@mail.ru>
2026-08-17 01:44:15 +0400

    feat: добавлены константы роутов приложения

---
+++ src/routes/pathConstants.ts
@@ -0,0 +1,9 @@
+export const ROUTES = {
+	HOME: "/",
+	MOVIES: "/movies",
+	MOVIE_DETAILS: "/movies/:id",
+	TV_SHOWS: "/tv",
+	TV_SHOWS_DETAILS: "/tv/:id",
+	PEOPLE: "/people",
+	WATCHLIST: "/watchlist",
+} as const;
