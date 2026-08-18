04ca4d5658387772f077385392f3864f8db2b647
CrawFish666 <emelyakoro4@mail.ru>
2026-08-18 18:07:16 +0400

    feat: Подключен tanstack query + devtools для него

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,20 @@
 import { BrowserRouter } from "react-router-dom"
 import { AppRoutes } from "./routes"
+import { QueryClientProvider } from "@tanstack/react-query"
+import { queryClient } from "./queryClient"
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools"
 
 
 function App() {
 
 
 	return (
-		<BrowserRouter>
-			<AppRoutes />
-		</BrowserRouter>
+		<QueryClientProvider client={queryClient}>
+			<BrowserRouter>
+				<AppRoutes />
+			</BrowserRouter>
+			<ReactQueryDevtools initialIsOpen={false} />
+		</QueryClientProvider>
 	)
 }
 
