b73b7c5c400226f81aa5020493a91ec8598ab2e7
CrawFish666 <emelyakoro4@mail.ru>
2026-09-01 12:13:37 +0400

    feat: Добавлен хук для подгрузки след. страниц в карусели

---
+++ src/hooks/useInfiniteScrollTrigger.ts
@@ -0,0 +1,52 @@
+import type { EmblaCarouselType } from "embla-carousel";
+import { useEffect } from "react";
+
+interface UseInfiniteScrollTriggerParams {
+	emblaApi: EmblaCarouselType | undefined;
+	// itemsCount: number;
+	hasNextPage: boolean | undefined;
+	isFetchingNextPage: boolean;
+	fetchNextPage: () => void;
+	screensAhead?: number; // за сколько "экранов" до конца начинать подгрузку
+}
+
+export function useInfiniteScrollTrigger({
+	emblaApi,
+	// itemsCount,
+	hasNextPage,
+	isFetchingNextPage,
+	fetchNextPage,
+	screensAhead = 2,
+}: UseInfiniteScrollTriggerParams) {
+
+	useEffect(() => {
+		if (!emblaApi) return;
+		const checkNearEnd = () => {
+			if (!hasNextPage || isFetchingNextPage) return;
+
+			const inView = emblaApi.slidesInView();
+			if (inView.length === 0) return;
+
+			const lastVisibleIndex = Math.max(...inView);
+			const visibleCount = inView.length;
+			const totalData = emblaApi.slideNodes().length;
+
+			const threshold = totalData - visibleCount * screensAhead - 1;
+
+			console.log("lastVisibleIndex:", lastVisibleIndex, "threshold:", threshold);
+			if (lastVisibleIndex >= threshold) {
+				fetchNextPage();
+			}
+		};
+
+
+
+		emblaApi.on("select", checkNearEnd);
+		emblaApi.on("reInit", checkNearEnd);
+
+		return () => {
+			emblaApi.off("select", checkNearEnd);
+			emblaApi.off("reInit", checkNearEnd);
+		};
+	}, [emblaApi, hasNextPage, isFetchingNextPage, fetchNextPage, screensAhead])
+}
