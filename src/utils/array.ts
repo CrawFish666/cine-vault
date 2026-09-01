275596a617669026e6c225aa035c34417fbc1ab7
CrawFish666 <emelyakoro4@mail.ru>
2026-09-01 11:57:17 +0400

    feat: Добавлена утилита удаление дубликатов по id

---
+++ src/utils/array.ts
@@ -0,0 +1,11 @@
+export function uniqueById<T extends { id: number }>(items: T[]): T[] {
+	return Array.from(
+		items.reduce((map, item) => {
+			if (!map.has(item.id)) {
+				map.set(item.id, item);
+			}
+
+			return map;
+		}, new Map<number, T>()).values()
+	);
+}
