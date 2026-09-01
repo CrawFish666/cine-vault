871b9f0024e71a7d99618b9781e475837d0f40e3
CrawFish666 <emelyakoro4@mail.ru>
2026-09-01 12:15:15 +0400

    feat: Добавлен хук для получения фильмов, которые сейчас в тренде

---
+++ src/hooks/useTrendingNowMovies.ts
@@ -0,0 +1,16 @@
+import { useInfiniteQuery,  } from "@tanstack/react-query";
+import { moviesApi, type TrendingNowParams } from "../api/movies";
+
+
+export function useTrendingNowMovies({ language = "ru-RU", time_window = "day" }: TrendingNowParams = {}) {
+	return useInfiniteQuery({
+		queryKey: ["movies", "trending-now", time_window, language],
+		queryFn: ({ pageParam }) => moviesApi.trendingNow({
+			language,
+			time_window,
+			page: pageParam
+		}),
+		initialPageParam: 1,
+		getNextPageParam: (lastPage) => lastPage.page < lastPage.total_pages ? lastPage.page + 1 : undefined,
+	})
+}
