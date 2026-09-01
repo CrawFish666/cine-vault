3cf846ec9a9c1c687592f9801582dcce6eaffa87
CrawFish666 <emelyakoro4@mail.ru>
2026-09-01 11:50:39 +0400

    feat: Добавлено api для мульти поиск и раздельного

---
+++ src/api/seacrh.ts
@@ -0,0 +1,46 @@
+import {type Movie, type MultiSearchResult, type Paginated, type Person, type TVShow } from "../types/tmdb";
+import { tmdbClient } from "./client";
+
+export interface MultiSearchParams {
+	query: string;
+	include_adult?: boolean;
+	language?: string;
+	page?: number;
+}
+
+export const searchApi = {
+	multi: ({
+		query,
+		include_adult = false,
+		language = "ru-RU",
+		page = 1,
+	}: MultiSearchParams) =>
+		tmdbClient
+			.get<Paginated<MultiSearchResult>>("/search/multi", {
+				params: {
+					query,
+					include_adult,
+					language,
+					page,
+				},
+			}).then(res => res.data),
+	movie: ({
+		query,
+		include_adult = false,
+		language = "ru-RU",
+		page = 1,
+	}: MultiSearchParams) => tmdbClient.get<Paginated<Movie>>("/search/movie", { params: { query, include_adult, language, page } }).then(res => res.data),
+	tv: ({
+		query,
+		include_adult = false,
+		language = "ru-RU",
+		page = 1,
+	}: MultiSearchParams) => tmdbClient.get<Paginated<TVShow>>("/search/tv", { params: { query, include_adult, language, page } }).then(res => res.data),
+	person: ({
+		query,
+		include_adult = false,
+		language = "ru-RU",
+		page = 1,
+	}: MultiSearchParams) => tmdbClient.get<Paginated<Person>>("/search/person", { params: { query, include_adult, language, page } }).then(res => res.data),
+
+};
