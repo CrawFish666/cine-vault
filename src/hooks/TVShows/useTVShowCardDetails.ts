2616c6b60b0269b9dc90a4024489f9688ca63e10
CrawFish666 <emelyakoro4@mail.ru>
2026-09-01 12:10:43 +0400

    feat: Добавлен хук для получения деталей о сериале в карточке

---
+++ src/hooks/TVShows/useTVShowCardDetails.ts
@@ -0,0 +1,16 @@
+import { useQueries } from "@tanstack/react-query";
+import type { TVShow, TVShowDetails } from "../../types/tmdb";
+import { tvshowsApi } from "../../api/tvshows";
+
+
+export function useTVShowCardDetails(tvShows: TVShow[], visibleIndexes: Set<number>) {
+	return useQueries({
+		queries: tvShows.map((show, index) => ({
+			queryKey: ["tvshows", "card-details", show.id],
+			queryFn: () => tvshowsApi.details(show.id),
+			// select: (data: TVShowDetails) => data.number_of_seasons,
+			staleTime: Infinity,
+			enabled: visibleIndexes.has(index)
+		})),
+	});
+}
