5ec59e5f793fbc501a3fd049a04cb1047d0b4ae0
CrawFish666 <emelyakoro4@mail.ru>
2026-09-01 12:18:46 +0400

    style: отформатировал AppLayout

--- src/layouts/AppLayout.tsx
+++ src/layouts/AppLayout.tsx
@@ -1,21 +1,22 @@
 import { Outlet } from "react-router-dom";
-import { Footer } from "../components/Footer";
+import { Footer } from "../components/Footer/Footer";
 import { Header } from "../components/Header/Header";
 
 
 
 export function AppLayout() {
+
+
+
 	return (
 		<>
-
-			<Header />
+			<Header/>
 
 			<main className="flex-1">
 				<Outlet />
 			</main>
 
 			<Footer />
-
 		</>
 	)
 }
