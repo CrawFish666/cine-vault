2aa5c0b169a92008b1234d635f00159d7c49202d
CrawFish666 <emelyakoro4@mail.ru>
2026-09-01 12:17:33 +0400

    feat: Добавлен хук для мульти-поиска

---
+++ src/hooks/Search/useSearchMulti.ts
@@ -0,0 +1,17 @@
+import { useInfiniteQuery } from "@tanstack/react-query";
+import { searchApi } from "../../api/seacrh";
+
+
+export function useSearchMulti(query: string) {
+	return useInfiniteQuery({
+		queryKey: ["search", "multi", query],
+		queryFn: ({ pageParam }) =>
+			searchApi.multi({
+				query,
+				page: pageParam,
+			}),
+		initialPageParam: 1,
+		getNextPageParam: (lastPage) => lastPage.page < lastPage.total_pages ? lastPage.page + 1 : undefined,
+		enabled: query.trim().length > 0,
+	});
+}
