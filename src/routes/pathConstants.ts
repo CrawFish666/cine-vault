7205dea9957fea1744b6f2b4861401175aeeb3e2
CrawFish666 <emelyakoro4@mail.ru>
2026-09-01 12:02:19 +0400

    feat: Обновлены маршруты приложения

--- src/routes/pathConstants.ts
+++ src/routes/pathConstants.ts
@@ -2,8 +2,11 @@ export const ROUTES = {
 	HOME: "/",
 	MOVIES: "/movies",
 	MOVIE_DETAILS: "/movies/:id",
-	TV_SHOWS: "/tv",
-	TV_SHOWS_DETAILS: "/tv/:id",
+	MOVIE_DETAILS_BY_ID: (id: number | string) => `/movies/${id}`,
+	TV_SHOWS: "/tv_shows",
+	TV_SHOWS_DETAILS: "/tv_shows/:id",
+	TV_SHOWS_DETAILS_BY_ID: (id: number | string) => `/tv/${id}`,
 	PEOPLE: "/people",
 	WATCHLIST: "/watchlist",
+	SEARCH: "/search",
 } as const;
