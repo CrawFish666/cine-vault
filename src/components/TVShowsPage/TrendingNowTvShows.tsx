7139ef83fe9fa8fd4b09c7a5fa079c7b3e8fb45a
CrawFish666 <emelyakoro4@mail.ru>
2026-09-04 18:12:08 +0400

    feat: Добавлена возможность выбора тренда за день/неделю. Навигация по выбранному медиа. Правильный паттерн нажатия добавления медиа в watchlist

--- src/components/TVShowsPage/TrendingNowTvShows.tsx
+++ src/components/TVShowsPage/TrendingNowTvShows.tsx
@@ -13,10 +13,15 @@ import { formatRuntime, formatVoteCount } from "../../utils/format";
 import { useVisibleSlides } from "../../hooks/useVisibleSlides";
 import { useTVShowCardDetails } from "../../hooks/TVShows/useTVShowCardDetails";
 import { Eye } from "lucide-react";
+import type { TimeWindowParam } from "../../api/tvshows";
+import { useState } from "react";
+import { ROUTES } from "../../routes/pathConstants";
 
 
 
 export function TrendingNowTvShows() {
+	const [timeWindow, setTimeWindow] = useState<TimeWindowParam["time_window"]>("day");
+
 	const timeZone = Intl.DateTimeFormat().resolvedOptions().timeZone;
 	console.log(timeZone); // Выведет текущую временную зону
 
@@ -24,7 +29,7 @@ export function TrendingNowTvShows() {
 		fetchNextPage,
 		hasNextPage,
 		isFetchingNextPage } = useTrendingNowTVShows({
-			time_window: "day"
+			time_window: timeWindow
 		})
 
 	const allData = uniqueById(data?.pages.flatMap(page => page.results) ?? []);
@@ -75,18 +80,43 @@ export function TrendingNowTvShows() {
 				scrollPrev={scrollPrev}
 				scrollNext={scrollNext}
 			/>
+			<div className="flex gap-3 mb-10">
+				<button
+					onClick={() => {
+						setTimeWindow("day")
+						emblaApi?.scrollTo(0);
+					}}
+					className={`px-6 py-3 rounded-lg transition-colors ${timeWindow === "day"
+						? "bg-primary-45 text-white cursor-not-allowed"
+						: "bg-surface-08 border border-surface-15 text-neutral-60 cursor-pointer"
+						}`}
+				>
+					Сегодня
+				</button>
+
+				<button
+					onClick={() => {
+						setTimeWindow("week")
+						emblaApi?.scrollTo(0);
+					}}
+					className={`px-6 py-3 rounded-lg transition-colors ${timeWindow === "week"
+						? "bg-primary-45 text-white cursor-not-allowed"
+						: "bg-surface-08 border border-surface-15 text-neutral-60 cursor-pointer"
+						}`}
+				>
+					Неделя
+				</button>
+			</div>
 			<Carousel emblaRef={emblaRef} className="gap-5">
 				{allData.map((item, index) => {
 					// const runtime = runtimes[index]?.data;
 					const details = detailsData[index].data;
 					return (
-						<CarouselSlide key={item.id} className="basis-[181px] sm:basis-[calc((100%-40px)/3)] md:basis-[calc((100%-60px)/4)] lg:basis-[calc((100%-80px)/5)]">
-							<CarouselCard className="flex flex-col p-3 h-[clamp(259px,calc(4.667vw+240.8px),308px)] laptop:h-[clamp(308px,calc(14.375vw+101px),377px)]">
+						<CarouselSlide key={item.id} className="relative basis-[181px] sm:basis-[calc((100%-40px)/3)] md:basis-[calc((100%-60px)/4)] lg:basis-[calc((100%-80px)/5)]">
+							<CarouselCard to={ROUTES.TV_SHOWS_DETAILS_BY_ID(item.id)} className="flex flex-col p-3 h-[clamp(259px,calc(4.667vw+240.8px),308px)] laptop:h-[clamp(308px,calc(14.375vw+101px),377px)]">
 								<div className="relative mb-3 flex-1 min-h-0">
 
-									<WatchlistButton
-										className="absolute right-3 top-3"
-										item={{ id: item.id, mediaType: "tv", posterPath: item.poster_path, title: item.name, voteAverage: item.vote_average }} />
+									
 									<img className="rounded-[10px] w-full h-full object-cover" src={`https://image.tmdb.org/t/p/original${item.poster_path}`} />
 								</div>
 
@@ -107,6 +137,9 @@ export function TrendingNowTvShows() {
 
 								</div>
 							</CarouselCard>
+							<WatchlistButton
+								className="absolute right-6 top-6"
+								item={{ id: item.id, mediaType: "tv", posterPath: item.poster_path, title: item.name, voteAverage: item.vote_average }} />
 						</CarouselSlide>
 					)
 				})}
