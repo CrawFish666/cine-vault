29afbab75a388fdb72a82f27d62d1c4f6c21ad31
CrawFish666 <emelyakoro4@mail.ru>
2026-09-04 17:54:05 +0400

    feat: Добавлен хук получения всех деталей о фильме с доп запросами

---
+++ src/hooks/useMovieDetails.ts
@@ -0,0 +1,10 @@
+import { useQuery } from "@tanstack/react-query";
+import { moviesApi } from "../api/movies";
+
+export function useMovieDetails(id: number) {
+	return useQuery({
+		queryKey: ["movies", "details", id],
+		queryFn: () => moviesApi.details({ id, language: "ru-RU", append_to_response: "credits,videos" }),
+		enabled: !!id
+	})
+}
