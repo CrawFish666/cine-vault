1d7e504ef5b87261ac699641a1320ca70fe74d43
CrawFish666 <emelyakoro4@mail.ru>
2026-09-04 18:11:17 +0400

    feat: Обернут весь контент в main контейнером

--- src/layouts/AppLayout.tsx
+++ src/layouts/AppLayout.tsx
@@ -1,22 +1,28 @@
 import { Outlet } from "react-router-dom";
 import { Footer } from "../components/Footer/Footer";
 import { Header } from "../components/Header/Header";
+import { PageContainer } from "../components/PageContainer";
+import { useScrollToHash } from "../hooks/useScrollToHash";
+import { useScrollToTop } from "../hooks/useScrollToTop";
 
 
 
 export function AppLayout() {
-
-
+	useScrollToTop()
+	useScrollToHash()
 
 	return (
-		<>
-			<Header/>
 
-			<main className="flex-1">
-				<Outlet />
+		<>
+			<Header />
+
+			<main className="flex-1 ">
+				<PageContainer>
+					<Outlet />
+				</PageContainer>
 			</main>
 
-			<Footer />
-		</>
+			<Footer /></>
+
 	)
 }
