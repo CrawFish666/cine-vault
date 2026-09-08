1c6dc79067eb8acbc8690fb42638a33e684ccb01
CrawFish666 <emelyakoro4@mail.ru>
2026-09-08 15:18:29 +0400

    feat: Соцсети в Footer теперь отдельные иконки вынесенные в отдельный компонент

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -2,22 +2,23 @@ import { Link } from "react-router-dom";
 import { navLinks } from "../Header/navLinks";
 import { sectionLinks } from "./sectionLinks";
 import { SectionLink } from "./SectionLink";
+import { SocialLinks } from "./SocialLinks";
 
 export function Footer() {
 
 	return (
 		<footer className="bg-surface-06 text-white">
 			<div className="w-full max-w-[1600px] mx-auto px-4 min-[1280px]:px-[clamp(16px,calc(15vw_-_176px),40px)] laptop:px-10 2xl:px-[clamp(0px,calc(-10.417vw_+_200px),40px)]">
-				<div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 mb-12.5   pt-20 pb-10">
+				<div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 mb-12.5 pt-20 pb-10 gap-5">
 					{navLinks.map((navLink) => {
 						const sections = sectionLinks[navLink.to] ?? [];
 
 						return (
-							<div key={navLink.to}>
+							<div key={navLink.to} className="flex flex-col items-start">
 								<Link to={navLink.to} className="text-white text-lg font-semibold inline-block mb-5">{navLink.label}</Link>
 
 								{sections.length > 0 && (
-									<div className="flex flex-col text-neutral-60 text-base font-medium gap-2.5">
+									<div className="flex flex-col items-start text-neutral-60 text-base font-medium gap-2.5">
 										{sections.map((section) => (
 											<SectionLink
 												key={section.id}
@@ -30,13 +31,9 @@ export function Footer() {
 							</div>
 						)
 					})}
-					<div>
-						<p>Connect With Us</p>
-						<div>
-							<div>twitter</div>
-							<div>facebook</div>
-							<div>linkedin</div>
-						</div>
+					<div className="col-span-full sm:col-span-1">
+						<p className="text-white text-lg font-semibold inline-block mb-5">Connect With Us</p>
+						<SocialLinks className="w-5 h-5"/>
 					</div>
 				</div>
 
